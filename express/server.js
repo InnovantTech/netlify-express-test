b0056dadba66a4b9578ac29cfce38479003297aa
Noé Landré <36192119+InnovantTech@users.noreply.github.com>
2020-03-06 20:43:21 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -5,20 +5,15 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-//const puppeteer = require('puppeteer');
-//var mysql = require('mysql2/promise');
-//var sanitizer = require('sanitizer');
-//var aes256 = require('./aes256');
-const serv = express().Router();
-
-serv.get('/',  async function(req, res) {
+const router = express.Router();
+router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js 23!</h1>');
+  res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', serv);  // path must route to lambda
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
