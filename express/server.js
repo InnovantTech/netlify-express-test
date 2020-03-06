607d36798546ec3b5f3706e59f741f016b027bbf
Noé Landré <36192119+InnovantTech@users.noreply.github.com>
2020-03-06 20:38:52 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -15,7 +15,7 @@ serv.get('/',  async function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js 23!</h1>');
   res.end();
-}
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', serv);  // path must route to lambda
